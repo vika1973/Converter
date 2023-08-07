f39cf3b6e89472f65f62340854b0b4bc422b7db1
vika1973 <viktoria.kurilo2k19@gmail.com>
2023-08-07 23:50:41 +0300

    Add uppercase h1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Converter';
+  title = 'Currency converter';
   currency1 : number = 0;
   currency2 : number = 0;
   result : number = 0;
